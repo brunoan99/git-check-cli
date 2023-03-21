306f9a34bce9d5c41a6e752401f817c35e344011
Bruno Andrade <brunoan99@gmail.com>
2023-03-21 19:21:13 -0300

    feat: add more cli options and commands

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,35 @@
+use std::path::PathBuf;
+
 use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
 pub struct Cli {
   #[command(subcommand)]
   pub command: Commands,
+
+  /// Turn verbose mode on
+  #[arg(short, long)]
+  pub verbose: bool,
+
+  /// Sets a custom config file
+  #[arg(short, long, value_name = "FILE")]
+  pub config: Option<PathBuf>,
+
+  /// Sets a custom project list file
+  #[arg(short, long, value_name = "FILE")]
+  pub paths: Option<PathBuf>,
 }
 
 #[derive(Subcommand)]
 pub enum Commands {
+  /// Check all projects being tracked
   Check,
+  /// Check a specific project
+  CheckPath { name: String },
+  /// Add a new project to the list of tracked projects
+  AddPath { path: String },
+  /// Remove a project from the list of tracked projects
+  RemovePath { path: String },
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,38 @@
+use std::path::{Path, PathBuf};
+
 use clap::Parser;
 use git_check_cli::{Cli, Commands};
 
 fn main() {
   let cli = Cli::parse();
 
+  let verbose = cli.verbose;
+
+  let config_file = if let Some(config_path) = cli.config.as_deref() {
+    config_path
+  } else {
+    Path::new("$HOME/.config/config.yaml")
+  };
+
+  let paths_file = if let Some(paths) = cli.paths.as_deref() {
+    paths
+  } else {
+    Path::new("$HOME/.config/projects.yaml")
+  };
+
   match &cli.command {
-    Commands::Check => println!("Check command is available"),
+    Commands::Check => {
+      //
+    }
+    Commands::CheckPath { name } => {
+      //
+    }
+    Commands::AddPath { path } => {
+      //
+    }
+    Commands::RemovePath { path } => {
+      //
+    }
+    _ => println!("another command"),
   }
 }
