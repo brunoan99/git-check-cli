7701c9ae35fcbb31373a1cfff81bd343ddcfb4e5
Bruno Andrade <brunoan99@gmail.com>
2023-03-21 18:44:47 -0300

    feat: start cli structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use clap::Parser;
+use git_check_cli::{Cli, Commands};
+
 fn main() {
-    println!("Hello, world!");
+  let cli = Cli::parse();
+
+  match &cli.command {
+    Commands::Check => println!("Check command is available"),
+  }
 }
